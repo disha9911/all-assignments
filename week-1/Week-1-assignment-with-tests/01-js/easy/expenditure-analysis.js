f620391eb94072ceeb40fbbef1bd94bd09171998
disha9911 <dishalohia.is22@bmsce.ac.in>
2023-11-07 16:09:35 +0530

    Solved Expenditure

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -7,9 +7,67 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-expenditure-analysis`
 */
+const transactions = [
+  {
+    id: 1,
+    timestamp: 1656076800000,
+    price: 10,
+    category: 'Food',
+    itemName: 'Pizza',
+  },
+  {
+    id: 2,
+    timestamp: 1656259600000,
+    price: 20,
+    category: 'Food',
+    itemName: 'Burger',
+  },
+  {
+    id: 3,
+    timestamp: 1656019200000,
+    price: 15,
+    category: 'Clothing',
+    itemName: 'T-Shirt',
+  },
+  {
+    id: 4,
+    timestamp: 1656364800000,
+    price: 30,
+    category: 'Electronics',
+    itemName: 'Headphones',
+  },
+  {
+    id: 5,
+    timestamp: 1656105600000,
+    price: 25,
+    category: 'Clothing',
+    itemName: 'Jeans',
+  },
+];
 
+
+var spendEstimates={}
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  for(var i=0;i<transactions.length;i++){
+   var t=transactions[i]
+  if(spendEstimates[t.category])
+   spendEstimates[t.category]+=t.price;
+  else
+   spendEstimates[t.category]=t.price;
+  }
+
+  var keys=Object.keys(spendEstimates)
+  let answer=[];
+  for(var i=0;i<keys.length;i++){
+    answer.push({
+      Catergory: keys[i],
+      TotalPrice: spendEstimates[keys[i]]
+    })
+  
+  }
+  return answer;
 }
 
-module.exports = calculateTotalSpentByCategory;
+var a=calculateTotalSpentByCategory(transactions);
+console.log(a)
+
